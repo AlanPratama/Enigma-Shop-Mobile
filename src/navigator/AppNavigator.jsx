fc7ade7fb14c9e6a45619e1e87227a5aa4ed3427
AlanPratama <pralan76@gmail.com>
2024-09-07 12:46:51 +0700

    [FEATURE]: make protected and public routes

--- src/navigator/AppNavigator.jsx
+++ src/navigator/AppNavigator.jsx
@@ -1,27 +1,33 @@
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import React from 'react'
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
-import LoginScreen from '../screens/LoginScreen'
-import WelcomeScreen from '../screens/WelcomeScreen'
-import HomeScreen from '../screens/authorization/HomeScreen'
-import RegisterScreen from '../screens/RegisterScreen'
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React, { useState } from "react";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import WelcomeScreen from "../screens/WelcomeScreen";
+import ProtectedRoutes from "./ProtectedRoutes";
+import PublicRoutes from "./PublicRoutes";
 
 export default function AppNavigator() {
-    const Stack = createNativeStackNavigator()
+  const Stack = createNativeStackNavigator();
+  const [isLogin, setIsLogin] = useState(true);
   return (
     <SafeAreaProvider>
-        <SafeAreaView className="flex-1">
-            <NavigationContainer>
-                <Stack.Navigator initialRouteName='Welcome' screenOptions={{headerShown: false}}>
-                    <Stack.Screen name='Welcome' component={WelcomeScreen} />
-                    <Stack.Screen name='Login' component={LoginScreen} />
-                    <Stack.Screen name='Register' component={RegisterScreen} />
-                    
-                    <Stack.Screen name='Home' component={HomeScreen} />
-                </Stack.Navigator>
-            </NavigationContainer>
-        </SafeAreaView>
+      <SafeAreaView className="flex-1">
+        <NavigationContainer>
+          <Stack.Navigator
+            screenOptions={{ headerShown: false }}
+            initialRouteName="Welcome"
+          >
+            <Stack.Screen name="Welcome" component={WelcomeScreen} />
+
+            {isLogin ? (
+                <Stack.Screen name="Protected" component={ProtectedRoutes} />
+            ) : (
+                <Stack.Screen name="Public" component={PublicRoutes} />
+            )}
+
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaView>
     </SafeAreaProvider>
-  )
+  );
 }
--- src/screens/authorization/product/ProductScreen.jsx
+++ src/screens/authorization/product/ProductScreen.jsx
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function ProducScreen() {
+export default function ProductScreen() {
   return (
     <View>
       <Text>ProducScreen</Text>
